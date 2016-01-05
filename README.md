df9f0e3a7cb8cafdc795d31bf93c0fc7b08c2e91
arcseldon <arcseldon@gmail.com>
2016-01-05 16:06:08 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# auth0-quick-hack
+
+Very quick demonstration of [wt-cli](https://www.npmjs.com/package/wt-cli) from [auth0](https://webtask.io).
+
+This app is pretty bare-bones. Does internally use the following popular NPM modules
+
+* async (waterfall)
+* lodash
+* request
+
+### To run
+
+Install [wt-cli](https://www.npmjs.com/package/wt-cli):
+
+From the root of this repo:
+
+```
+> $ wt create githubInfo.js --name github-info
+```
+
+Use that response URL with a username query param suffixed to call the created webtask
+
+#### Example execution
+
+with response URL: https://webtask.it.auth0.com/api/run/wt-arcseldon-gmail_com-0/github-info
+
+With github username: "angular"
+
+```
+> $  curl https://webtask.it.auth0.com/api/run/wt-arcseldon-gmail_com-0/github-info?username=angular
+```
+
+This app is a quick hack only, there are many limitations etc. However, it is easy to see how this could
+be extended and potentially used within a web front-end. For example a simple Angular app with a search box for
+username. Then results are presented with the user information and github avatar image, together with a
+list of matching repos beneath.
+
+Bear in mind that although the Github API is already public, one advantage of using the webtask approach is taking
+computation offline, and allowing the webtask to provide a custom "aggregated data response" internally having potentially
+called many different API URLs.
+
+
---
+++ githubInfo.js
@@ -0,0 +1,87 @@
+'use strict';
+
+/*
+ Connects to public github api.
+ The username and (optionally) minStars should be provided via query params
+ */
+
+var request = require('request'),
+  waterfall = require('async').waterfall,
+  _ = require('lodash');
+
+module.exports = function (ctx, done) {
+
+  // fetch username to query & optionally minimum star count
+  // quick hack, limited validation
+  var username = ctx.data.username,
+    minStars = ctx.data.minStars || 1;
+
+  if (!username) {
+    return done('username is required - must be the github username to query');
+  }
+
+  if (!_.isNumber(minStars) || minStars < 0) {
+    return done('Illegal minStars - must be integer greater than or equal to zero');
+  }
+
+  waterfall([
+
+    function getUserInfo(done) {
+
+      request({
+        method: 'GET',
+        url: 'https://api.github.com/users/' + username,
+        headers: {
+          'User-Agent': 'request'
+        }
+      }, function (error, response, body) {
+
+        if (error) {
+          return done(err);
+        }
+        if (response.statusCode !== 200) {
+          return done(err);
+        }
+        // parse out required attributes
+        var userInfo = JSON.parse(body);
+        done(null, {name: userInfo.name, location: userInfo.location, email: userInfo.email});
+      });
+
+    },
+    function getUserRepos(user, done) {
+      request({
+        method: 'GET',
+        url: 'https://api.github.com/users/' + username + '/repos?per_page=100',
+        headers: {
+          'User-Agent': 'request'
+        }
+      }, function (error, response, body) {
+
+        if (error) {
+          return done(err);
+        }
+        if (response.statusCode !== 200) {
+          return done(err);
+        }
+        // filter and transform repo data to required format
+        var reposInfo = JSON.parse(body),
+          repos = _(reposInfo)
+            .filter({fork: false, private: false})
+            .filter(function (repo) {
+              return repo.stargazers_count >= minStars;
+            })
+            .map(function (repo) {
+              return {name: repo.full_name, stars: repo.stargazers_count};
+            })
+            .value();
+
+        done(null, {user: user, repos: repos});
+
+      });
+    }
+  ], function (error, data) {
+    // final callback, return error or result
+    return (error) ? done(error) : done(null, data);
+  });
+
+};
